b4ede2669d18530b41e2c705cc867fe6cf5c01e7
segerpeter07 <segerpeter07@gmail.com>
2017-10-18 21:17:12 -0400

    Got the webapp working, working on the backend, overall good forward progress

---
+++ backend.py
@@ -0,0 +1,69 @@
+import csv
+
+def build_dict(filename):
+    file = open(filename, 'r')
+    ans = {}
+    for line in file:
+        data = line.split(', ')
+        ans[data[0]] = data[1].split('\n')[0]
+    return ans
+
+def build_playlist(songs, desired_bpm):
+    target_bpms = list(range(desired_bpm-10, desired_bpm+10))
+    ans = []
+    for key in songs:
+        bpm = songs[key]
+        if int(bpm) in target_bpms:
+            ans.append(key)
+    return ans
+
+def readin_csv(filename):
+    f = open('uploads/'+filename, 'r')
+    reader = csv.reader(f)
+    ans = []
+    i = 0
+    for row in reader:
+        if i > 0:
+            # ans.append(row)
+            tmp = []
+
+            if row[0] != '' and row[10] != '' and row[11] != '' and row[12] != '' \
+                and row[7] != '' and row[8] != '' and row[9] != ''\
+                and row[4] != '' and row[5] != '' and row[6] != '':
+                    time = float((row[0]))
+                    raw_accel_x = float(row[10])
+                    raw_accel_y = float(row[11])
+                    raw_accel_z = float(row[12])
+                    g_x = float(row[7])
+                    g_y = float(row[8])
+                    g_z = float(row[9])
+                    rot_x = float(row[4])
+                    rot_y = float(row[5])
+                    rot_z = float(row[6])
+
+
+                    accel_x = (raw_accel_x)*-9.8+(g_x)
+                    accel_y = (raw_accel_y)*-9.8*(g_y)
+                    accel_z = (raw_accel_z)*-9.8*(g_z)
+
+                    accel = [time, accel_x, accel_y, accel_z]
+                    gyro = [time, rot_x, rot_y, rot_z]
+
+                    tmp.append(accel)
+                    tmp.append(gyro)
+                    ans.append(tmp)
+        i += 1
+    f.close()
+    print(ans[1])
+
+def readin_txt(filename):
+    file = open('uploads/'+filename, 'r')
+    ans = []
+    for line in file:
+        ans.append(line)
+    print(ans[2])
+
+
+if __name__ == '__main__':
+    # readin_txt('Recording1.txt')
+    readin_csv('testrun1.csv')
--- fitspot.py
+++ fitspot.py
@@ -1,32 +1,74 @@
 import os
 import requests
+import spotipy
+from flask import Flask, redirect, render_template, request, session, abort, url_for
+from werkzeug.utils import secure_filename
+from backend import *
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'csv'])
 
 key_id = os.environ.get('SPOTIFY_CLIENT_ID')
 key_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
 
-def build_dict(filename):
-    file = open(filename, 'r')
-    ans = {}
-    for line in file:
-        data = line.split(', ')
-        ans[data[0]] = data[1].split('\n')[0]
-    return ans
+sp = spotipy.Spotify(key_secret)
 
-def build_playlist(songs, desired_bpm):
-    target_bpms = list(range(desired_bpm-10, desired_bpm+10))
-    ans = []
-    for key in songs:
-        bpm = songs[key]
-        if int(bpm) in target_bpms:
-            ans.append(key)
-    return ans
+app = Flask('flaskapp')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    # url = 'https://accounts.spotify.com/authorize/?client_id=' + key_id + '&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%2Fcallback'
+    # return render_template('home.html')
+
+    if request.method == 'POST':
+        # Check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            # return redirect(url_for('upload_file', filename=filename))
+            return filename
+    return render_template('index.html')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
 if __name__ == "__main__":
     dic = build_dict('top40')
     # print(dic)
     playlist = build_playlist(dic, 130)
-    print(playlist)
+    # print(playlist)
 
-    res = requests.get("https://api.spotify.com/v1/search?q=album:arrival%20artist:abba&type=album", auth=(key_id, key_secret))
-    data = res.json()
-    print(data)
+    # results = sp.search(q='weezer', limit=20)
+    # for i, t in enumerate(results['tracks']['items']):
+    #     print(' ', i, t['name'])
+
+
+    app.secret_key = os.urandom(12)
+    HOST = 'localhost'
+    # HOST = '10.7.68.124'
+    PORT = int(os.environ.get('PORT', 5000))
+    app.run(host=HOST, port=PORT)
+
+    # res = requests.get("https://api.spotify.com/v1/search?q=album:arrival%20artist:abba&type=album", -H "Authorization: Bearer {key_id}")
+    # res = requests.get("https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/top-tracks?country=United+States", auth=(key_id, key_secret))
+    # print(res)
+    # url = 'https://accounts.spotify.com/authorize/?client_id=' + key_id + '&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%2Fcallback'
+    # r = requests.get(url)
+    # print(r.text)
+    # r = requests.get('https://api.spotify.com/v1/search?q=album:arrival%20artist:abba&type=album')
+    # print(r.text)
+    # print(res)
+    # data = res.json()
+    # print(data)
