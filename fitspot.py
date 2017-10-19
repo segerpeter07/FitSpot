02948be812a91e5ee7eb74e6d8a8bbb2337796f2
segerpeter07 <segerpeter07@gmail.com>
2017-10-19 15:01:31 -0400

    Got Spotify working

--- fitspot.py
+++ fitspot.py
@@ -1,9 +1,17 @@
 import os
+import sys
 import requests
 import spotipy
 from flask import Flask, redirect, render_template, request, session, abort, url_for
 from werkzeug.utils import secure_filename
 from backend import *
+import base64
+
+import spotipy
+import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials
+
+scope = 'user-library-read'
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'csv'])
@@ -48,18 +56,40 @@ if __name__ == "__main__":
     dic = build_dict('top40')
     # print(dic)
     playlist = build_playlist(dic, 130)
-    # print(playlist)
+    # print(playlist)\
 
-    # results = sp.search(q='weezer', limit=20)
-    # for i, t in enumerate(results['tracks']['items']):
-    #     print(' ', i, t['name'])
+    '''
+    key_id = key_id.encode('ascii')
+    key_secret = key_secret.encode('ascii')
+
+    auth_id = base64.b64encode(key_id)
+    auth_key = base64.b64decode(key_secret)
+
+    # print(auth_id)
+    authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+            'Authorization': 'Basic ' + auth_id + ':' + auth_key
+        }
+        form: {
+            grant_type: 'client_credentials'
+        }
+        json: True
+    }
+    # header = 'Authorization': 'Basic ' + auth_id + ':' + auth_key
+    # print(authOptions)
+
+    # r = requests.post(url='https://accounts.spotify.com/api/token', headers=header, grant_type='client_credentials', json=True)
+    r = requests.post(authOptions)
+    print(r)
+    '''
 
 
-    app.secret_key = os.urandom(12)
-    HOST = 'localhost'
-    # HOST = '10.7.68.124'
-    PORT = int(os.environ.get('PORT', 5000))
-    app.run(host=HOST, port=PORT)
+    # app.secret_key = os.urandom(12)
+    # HOST = 'localhost'
+    # # HOST = '10.7.68.124'
+    # PORT = int(os.environ.get('PORT', 5000))
+    # app.run(host=HOST, port=PORT)
 
     # res = requests.get("https://api.spotify.com/v1/search?q=album:arrival%20artist:abba&type=album", -H "Authorization: Bearer {key_id}")
     # res = requests.get("https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/top-tracks?country=United+States", auth=(key_id, key_secret))
@@ -72,3 +102,41 @@ if __name__ == "__main__":
     # print(res)
     # data = res.json()
     # print(data)
+
+
+
+
+    client_credentials_manager = SpotifyClientCredentials(client_id=key_id, client_secret=key_secret)
+
+    '''
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    # spotify = spotipy.Spotify()
+    res = sp.search(q='artist:MIKA', type='artist')
+    print(res)
+    '''
+
+    if len(sys.argv) > 1:
+        username = sys.argv[1]
+    else:
+        print("Usage: %s username" % (sys.argv[0],))
+        sys.exit()
+
+    token = util.prompt_for_user_token(username, scope)
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        results = sp.current_user_saved_tracks()
+        for item in results['items']:
+            track = item['track']
+            print(track['name'] + ' - ' + track['artists'][0]['name'])
+    else:
+        print("Can't get token for", username)
+
+    # playlists = sp.user_playlists('lightknight')
+    # while playlists:
+    #     for i, playlist in enumerate(playlists['items']):
+    #         print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+    #     if playlists['next']:
+    #         playlists = sp.next(playlists)
+    #     else:
+    #         playlists = None
